317b9495b6122480b0952c41b1ac1eac39bde851
Michael Plyler <mplyler@fhu.edu>
2017-07-14 00:01:43 -0500

    updates to database example

--- code-examples/db-connect.php
+++ code-examples/db-connect.php
@@ -14,5 +14,26 @@
     Step 4: close connection
 -->
 
+<?php
+    // Step 1
+    $db = mysqli_connect('localhost','username','password','dbname')
+    or die('Error connecting to MySQL server.');
+
+    // Step 2
+    $query = "SELECT * FROM `table`";
+    mysqli_query($db,$query) or die('Error querying database.');
+
+    $result = mysqli_query($db,$query);
+
+    // Step 3
+    while ($row = mysqli_fetch_array($result)){
+        echo $row['column1'] . ' ' . $row['column2'] . '<br/>';
+    }
+
+    // Step 4
+    mysqli_close($db);
+
+?>
+
 </body>
 </html>
